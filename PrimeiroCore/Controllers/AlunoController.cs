52fb2f1cc26952095bb8d6f622d0be8cd0a7aac1
Pedro Vieira <pevieiraf@gmail.com>
2024-03-20 19:17:51 -0300

    Primeiro commit

---
+++ PrimeiroCore/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiroCore.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
